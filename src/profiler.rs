506c4226e71aca28d1df507fd1b41c400725169d
Dallin Wright <dallinwright@tradecrit.com>
2024-12-20 09:01:56 -0800

    test

--- src/profiler.rs
+++ src/profiler.rs
@@ -2,7 +2,7 @@ use pyroscope::pyroscope::PyroscopeAgentReady;
 use pyroscope::PyroscopeAgent;
 use pyroscope_pprofrs::{pprof_backend, PprofConfig};
 
-pub fn create(url: &str, service_name: String) -> Result<PyroscopeAgent<PyroscopeAgentReady>, Box<dyn std::error::Error>> {
+pub fn create(url: &str, service_name: &str) -> Result<PyroscopeAgent<PyroscopeAgentReady>, Box<dyn std::error::Error>> {
     let pprof_config = PprofConfig::new()
         .sample_rate(100)
         .report_thread_id()
@@ -10,9 +10,12 @@ pub fn create(url: &str, service_name: String) -> Result<PyroscopeAgent<Pyroscop
 
     let backend_impl = pprof_backend(pprof_config);
 
+    let tags: Vec<(&str, &str)> = vec![("service.name", service_name), ("language", "rust")];
+
     // Configure Pyroscope Agent
-    let agent = PyroscopeAgent::builder(url, &service_name)
+    let agent = PyroscopeAgent::builder(url, service_name)
         .backend(backend_impl)
+        .tags(tags)
         .build()
         .expect("Failed to create Pyroscope Agent");
 
